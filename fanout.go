554301f7f6a2c1e7030c8ce808744930cccefcd9
Kon <bokarius@comcast.net>
2015-04-05 12:31:53 -0700

    added documentation

--- fanout.go
+++ fanout.go
@@ -9,10 +9,15 @@ package fanout
 
 import "github.com/fanout/go-pubcontrol"
 
+// The Fanout struct is used for publishing messages to Fanout.io and is
+// configured with a Fanout.io realm and associated key. SSL can either
+// be enabled or disabled.
 type Fanout struct {
     pubControl *pubcontrol.PubControl
 }
 
+// Initialize with a specified realm, key, and a boolean indicating wther
+// SSL should be enabled or disabled.
 func NewFanout(realm string, key []byte, ssl bool) *Fanout {
     fanout := new(Fanout)
     protocol := "https://"
@@ -27,6 +32,9 @@ func NewFanout(realm string, key []byte, ssl bool) *Fanout {
     return fanout
 }
 
+// Publish the specified data to the specified channel for the configured
+// Fanout.io realm. Optionally provide an ID and previous ID to send along
+// with the message.
 func (fanout *Fanout) Publish(channel string, data, id, prev_id string) error {
     format := &JsonObjectFormat{value: data} 
     item := pubcontrol.NewItem([]pubcontrol.Formatter{format}, id, prev_id)
--- jsonobjectformat.go
+++ jsonobjectformat.go
@@ -7,14 +7,18 @@
 
 package fanout
 
+// The JSON object format used for publishing messages to Fanout.io. The
+// Value field represents the data to be published.
 type JsonObjectFormat struct {
     value string
 }
 
+// The name of the format.
 func (format *JsonObjectFormat) Name() string {
     return "json-object"
 }
 
+// The method used to export the format data.
 func (format *JsonObjectFormat) Export() interface{} {
     return format.value
 }
